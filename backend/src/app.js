5528cc371eef810fa7a82b1fc1d9dda376d06fea
Psy <giulianolourencon@gmail.com>
2020-04-13 15:18:00 -0300

    Deploy and method override

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,5 +1,6 @@
 const express = require('express'); // Server structure
 const bodyParser = require('body-parser'); // Parse request bodies
+const methodOverride = require('method-override'); // Enable to use PUT and DELETE methods
 const cors = require('cors'); // Cross-Origin Resource Sharing - HTTP headers
 const { errors } = require('celebrate'); // Validation
 const db = require('./database/connection');
@@ -10,6 +11,7 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(cors());
 app.use(express.json());
+app.use(methodOverride("_method"));
 app.use(routes);
 app.use(errors);
 
