3dc642e79ef85bd780bd15ab4236c97394478f6b
Psy <giulianolourencon@gmail.com>
2020-05-07 17:47:27 -0300

    Error with dependencde dotenv fully fizxed

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,18 +1,20 @@
 const express = require('express'); // Server structure
-const bodyParser = require('body-parser'); // Parse request bodies
-const methodOverride = require('method-override'); // Enable to use PUT and DELETE methods
+const bodyParser = require('body-parser'); // Parse request bodies 
 const cors = require('cors'); // Cross-Origin Resource Sharing - HTTP headers
-const { errors } = require('celebrate'); // Validation
+const {
+    errors
+} = require('celebrate'); // Validation
 
 const db = require('./database/connection'); // Var only used for the db connection, plz dont delete
 const routes = require('./routes'); // Backend routes
 
 const app = express();
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+    extended: true
+}));
 app.use(cors());
 app.use(express.json());
-app.use(methodOverride("_method"));
 app.use(routes);
 app.use(errors);
 
